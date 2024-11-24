8bdc03fbc68a9f838c33192d9ab0456f1e9f4e20
Rida <rida.fatima1@ontariotechu.net>
2024-11-24 01:24:31 -0500

    update db.js

--- INFRProject/server/config/db.js
+++ INFRProject/server/config/db.js
@@ -1,11 +1,4 @@
-const mongoose = require('mongoose');
-
-// Replace this with your actual MongoDB URI, for example, `mongodb://localhost:27017/workouts`
-const dbURI = "mongodb://localhost:27017/workouts"; // Use your actual database name
-
-// Connect to MongoDB
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected successfully"))
-  .catch(err => console.log("MongoDB connection error: " + err));
-
-module.exports = mongoose;
+module.exports = {
+    URI: "mongodb://localhost:27017/workouts" // or your MongoDB URI here
+  };
+  
