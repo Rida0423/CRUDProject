2476bfdef2beab7f4603882e54cad0d5e709c1ad
Rida <rida.fatima1@ontariotechu.net>
2024-11-24 21:05:47 -0500

    update app.js

--- INFRProject/public/Script/app.js
+++ INFRProject/public/Script/app.js
@@ -1,8 +1,6 @@
-// immediately invoked function expression
-
 (function(){
     function start(){
-        console.log("app started")
+        console.log("app started");
     }
-    window.addEventListener("load",start);
+    window.addEventListener("load", start);
 })();
